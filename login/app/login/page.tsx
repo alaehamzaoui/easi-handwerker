6a3d9d505827e170174636db556540b33a655d82
Tedy-StevenFoyo-Nde <tedy-steven.foyo-nde@stud.hs-bochum.de>
2024-06-19 15:02:19 +0200

    style login sign up

--- login/app/login/page.tsx
+++ login/app/login/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 import Link from 'next/link';
 import { useState } from 'react';
-
+import styles from './page.module.css'; 
 export default function Login() {
   
-    const [email, setEmail] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = (e: { preventDefault: () => void; }) => {
@@ -12,7 +12,7 @@ export default function Login() {
     const userDataString = localStorage.getItem(email);
     let userData;
     if (userDataString) {
-    userData = JSON.parse(userDataString);
+      userData = JSON.parse(userDataString);
     }
     if (userData && userData.password === password) {
       alert('Login erfolreich!');
@@ -21,24 +21,39 @@ export default function Login() {
     }
   };
 
-
   return (
-    <div>
-      <h1><strong>Login</strong></h1>
-      <form onSubmit={handleLogin}>
+    <div className={styles.mainContainer}>
+    <div className={styles.container}>
+      <h1 className={styles.title}><strong>Login</strong></h1>
+      <form onSubmit={handleLogin} className={styles.form}>
         <div>
-            <input type='text' name="password" placeholder='Email' value={email}
-            onChange={(e) => setEmail(e.target.value)}/>
+          <input 
+            type='text' 
+            name="email" 
+            placeholder='Email' 
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className={styles.input}
+          />
         </div>
         <div>
-            <input type='text' name="password" placeholder='Passwort' value={password}
-            onChange={(e) => setPassword(e.target.value)}/>
+          <input 
+            type='password' 
+            name="password" 
+            placeholder='Passwort' 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className={styles.input}
+          />
         </div>
-        <button type='submit'>Login</button> 
-        </form>
-        <p>kein Account ?  <Link href="/signup"><button><strong>Registrierung</strong></button>
-      </Link> </p>
-      
+        <button type='submit' className={styles.button}>Login</button>
+      </form>
+      <p className={styles.registerText}>kein Account ?  
+        <Link href="/signup">
+          <button className={styles.registerButton}><strong>Registrierung</strong></button>
+        </Link>
+      </p>
+    </div>
     </div>
   );
 }
--- login/app/signup/page.tsx
+++ login/app/signup/page.tsx
@@ -1,15 +1,19 @@
 "use client"
 import Link from 'next/link';
 import { useState } from 'react';
+import styles from './page.module.css';
 
 export default function SignUp() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [birthDate, setBirthDate] = useState('');
+    const [category, setCategory] = useState('');
+    const [street, setStreet] = useState('');
     const [city, setCity] = useState('');
     const [phone, setPhone] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [passwordAgain, setPasswordAgain] = useState('');
 
     const handleSubmit = (e: { preventDefault: () => void; }) => {
         e.preventDefault();
@@ -17,6 +21,7 @@ export default function SignUp() {
           firstName,
           lastName,
           birthDate,
+          category,
           city,
           phone,
           email,
@@ -28,64 +33,87 @@ export default function SignUp() {
       };
   
     return (
-      <div>
-        <h1><strong>Registrierung</strong></h1>
+      <div className={styles.mainContainer}>
+      <div className={styles.container}>
+        <h1 className={styles.title}><strong>Registrierung</strong></h1>
         <form onSubmit={handleSubmit}>
           <label htmlFor="firstName"></label>
           <input
             type="text"
             id="firstName"
             value={firstName} placeholder='Vorname'
-            onChange={(e) => setFirstName(e.target.value)}
+            onChange={(e) => setFirstName(e.target.value)} className={styles.input}
           />
           <div><label htmlFor="lastName"></label>
           <input
             type="text"
             id="lastName"
             value={lastName} placeholder='Nachname'
-            onChange={(e) => setLastName(e.target.value)}
+            onChange={(e) => setLastName(e.target.value)} className={styles.input}
           /></div>
           <div><label htmlFor="birthDate"></label>
           <input
-            type="date"
+            type="text"
             id="birthDate"
             value={birthDate} placeholder='Geburtsdatum'
-            onChange={(e) => setBirthDate(e.target.value)}
+            onChange={(e) => setBirthDate(e.target.value)} className={styles.input}
+          /></div>
+          <div><label htmlFor="category"></label>
+          <input
+            type="text"
+            id="category"
+            value={category} placeholder='Art der Ausbildung'
+            onChange={(e) => setCategory(e.target.value)} className={styles.input}
+          /></div>
+          <div><label htmlFor="street"></label>
+          <input
+            type="text"
+            id="street"
+            value={street} placeholder='Straße & Hausnummer'
+            onChange={(e) => setStreet(e.target.value)} className={styles.input}
           /></div>
           <div><label htmlFor="city"></label>
           <input
             type="text"
             id="city"
             value={city} placeholder='PLZ Stadt'
-            onChange={(e) => setCity(e.target.value)}
+            onChange={(e) => setCity(e.target.value)} className={styles.input}
           /></div>
           <div><label htmlFor="phone"></label>
           <input
             type="tel"
             id="phone"
             value={phone} placeholder='telefonnummer'
-            onChange={(e) => setPhone(e.target.value)}
+            onChange={(e) => setPhone(e.target.value)} className={styles.input}
           /></div>
           <div><label htmlFor="email"></label>
           <input
             type="email"
             id="email"
             value={email} placeholder='Email'
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)} className={styles.input}
           /></div>
           <div><label htmlFor="password"></label>
           <input
             type="password"
             id="password"
             value={password} placeholder='Passwort'
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)} className={styles.input}
           /></div>
-          <button type="submit">Registrieren</button>
+          <div><label htmlFor="passwordAgain"></label>
+          <input
+            type="password"
+            id="passwordAgain"
+            value={passwordAgain} placeholder='Passwort bestätigen'
+            onChange={(e) => setPasswordAgain(e.target.value)} className={styles.input}
+          /></div>
+          <button type="submit" className={styles.button}>Registrieren</button>
         </form>
       <Link href="/login">
-        <button><strong>Login</strong>
-        </button>
+        <p className={styles.registerText}>schon ein Account? <button className={styles.loginButton}><strong>Login</strong>
+        </button></p>
       </Link>
+    </div>
     </div>
   );
 }
